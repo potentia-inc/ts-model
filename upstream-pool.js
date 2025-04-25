a89728336f55592420ea21d31a12ccee481300ea
Tz-Huan Huang <tzhuan@potentia.asia>
2025-04-25 13:33:59 +0800

    Release 3.1.0

--- upstream-pool.js
+++ upstream-pool.js
@@ -1,149 +1,50 @@
 import assert from 'node:assert';
 import { debug } from 'node:util';
 import { NoUpstreamError } from './error/upstream.js';
-import { Model, Models, UUID_DOC_SCHEMA, pickId, pickIdOrNil, toUnsetOrNil, } from './model.js';
-import { Nil, toUuid, isNullish } from './type.js';
-import { msleep, option } from './util.js';
+import { pickId, pickIdOrNil } from './model.js';
+import { isNullish } from './type.js';
+import { Upstream } from './upstream.js';
+import { msleep } from './util.js';
 const DEBUG = debug('potentia:model:upstream');
 const DEBUG_VERBOSE = debug('potentia:model:upstream:verbose');
-export const UPSTREAM_NAME = 'upstreams';
-export class Upstream extends Model {
-    type;
-    host;
-    path;
-    headers;
-    searchs;
-    auth;
-    interval;
-    weight;
-    url(options = {}) {
-        const url = new URL(this.host);
-        const path = options.path ?? this.path;
-        if (!isNullish(path)) {
-            if (!url.pathname.endsWith('/'))
-                url.pathname += '/';
-            url.pathname += path;
-        }
-        for (const [k, v] of new URLSearchParams({
-            ...this.searchs,
-            ...options.searchs,
-        })) {
-            url.searchParams.append(k, v);
-        }
-        return url;
-    }
-    link(options = {}) {
-        return this.url(options).toString();
-    }
-    constructor(doc) {
-        super(doc);
-        this.type = doc.type;
-        this.host = doc.host;
-        this.path = doc.path;
-        this.headers = doc.headers ?? {};
-        this.searchs = doc.searchs ?? {};
-        this.auth = doc.auth ?? {};
-        this.interval = doc.interval ?? 0.001;
-        this.weight = doc.weight ?? 0;
-    }
-}
-export const UPSTREAM_SCHEMA = {
-    name: UPSTREAM_NAME,
-    validator: {
-        $jsonSchema: {
-            type: 'object',
-            additionalProperties: false,
-            required: ['_id', 'type', 'host', 'created_at'],
-            properties: {
-                ...UUID_DOC_SCHEMA,
-                type: { type: 'string' },
-                host: { type: 'string' },
-                path: { type: 'string' },
-                headers: { type: 'object' },
-                searchs: { type: 'object' },
-                auth: { type: 'object' },
-                interval: { type: 'number' },
-                weight: { type: 'number' },
-            },
-        },
-    },
-    indexes: {
-        create_index: { keys: { created_at: 1 } },
-        type_index: {
-            keys: { type: 1, weight: 1 },
-        },
-    },
-};
-export class Upstreams extends Models {
-    get name() {
-        return UPSTREAM_NAME;
-    }
-    $model(doc) {
-        return new Upstream(doc);
-    }
-    $sort(sort) {
-        return isNullish(sort) ? Nil : { ...option('created_at', sort.createdAt) };
-    }
-    $query(query) {
-        const { gtWeight, gteWeight } = query;
-        return {
-            _id: pickIdOrNil(query.id),
-            type: pickIdOrNil(query.type),
-            weight: isNullish(gtWeight) && isNullish(gteWeight)
-                ? Nil
-                : {
-                    $gt: gtWeight,
-                    $gte: gteWeight,
-                },
-        };
-    }
-    $insert(values) {
-        const { type, host, path, headers, searchs, auth, interval, weight } = values;
-        assertInterval(interval);
-        assertWeight(weight);
-        const _id = values.id ?? toUuid();
-        return { _id, type, host, path, headers, searchs, auth, interval, weight };
-    }
-    $set(values) {
-        const { type, host, path, headers, searchs, auth, interval, weight } = values;
-        assertInterval(interval);
-        assertWeight(weight);
-        return { type, host, path, headers, searchs, auth, interval, weight };
-    }
-    $unset(values) {
-        return {
-            path: toUnsetOrNil(values, 'path'),
-            headers: toUnsetOrNil(values, 'headers'),
-            searchs: toUnsetOrNil(values, 'searchs'),
-            auth: toUnsetOrNil(values, 'auth'),
-            interval: toUnsetOrNil(values, 'interval'),
-            weight: toUnsetOrNil(values, 'weight'),
-        };
-    }
-}
-function assertInterval(x) {
-    if (!isNullish(x))
-        assert(x >= 0.001);
-}
-function assertWeight(x) {
-    if (!isNullish(x))
-        assert(x >= 0);
-}
-/*
- * @deprecated Use UpstreamPool instead
- */
-export class Pool {
-    #upstreams;
-    #type;
+export class UpstreamPool {
     #options;
+    #pools = new Map();
+    constructor(options) {
+        this.#options = options;
+    }
+    async sample(type, hint) {
+        const pool = await this.#pool(type);
+        return pool.sample(hint);
+    }
+    succeed(upstream) {
+        this.#pools.get(upstream.type)?.succeed(upstream);
+    }
+    fail(upstream) {
+        this.#pools.get(upstream.type)?.fail(upstream);
+    }
+    async #pool(type) {
+        const pool = this.#pools.get(type);
+        if (!isNullish(pool))
+            return pool;
+        const created = new Pool({
+            load: () => this.#options.load(type),
+            ...this.#options.init?.(type),
+        });
+        this.#pools.set(type, created);
+        return created;
+    }
+}
+class Pool {
+    #options;
+    #load;
     #caches = [];
     #failures = new Map();
     #times = new Map();
     #weights = new Map();
     #expiresAt = 0;
-    constructor(upstreams, type, options = {}) {
-        this.#upstreams = upstreams;
-        this.#type = type;
+    constructor(options) {
+        this.#load = options.load;
         this.#options = {
             ttl: options.ttl ?? 60,
             minFailures: options.minFailures ?? 0,
@@ -218,14 +119,10 @@ export class Pool {
             DEBUG(`sync: ignored`);
             return;
         }
-        const upstreams = await this.#upstreams.findMany({
-            type: this.#type,
-            gtWeight: 0,
-        });
+        const upstreams = await this.#load();
         const keys = new Set();
-        upstreams.forEach((x) => {
+        for await (const x of upstreams)
             keys.add(this.#key(x));
-        });
         // remove upstreams
         for (const x of this.#caches) {
             const key = this.#key(x);
@@ -260,4 +157,4 @@ export class Pool {
         return this.#weights.get(key) ?? weight;
     }
 }
-//# sourceMappingURL=upstream.js.map
+//# sourceMappingURL=upstream-pool.js.map
